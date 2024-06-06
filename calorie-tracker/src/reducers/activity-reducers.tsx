a343da3cc8a80444377374173535fbaa44f8c930
Oscar E. Oceguera Bibriesca <oscar.oceguera@payclip.com>
2024-06-05 23:40:20 -0700

    Calorie tracker basic

---
+++ calorie-tracker/src/reducers/activity-reducers.tsx
@@ -0,0 +1,73 @@
+import { Activity } from "../types";
+
+export type ActivityActions =
+  | {
+      type: "save-activity";
+      payload: { newAcitvity: Activity };
+    }
+  | { type: "set-activeId"; payload: { id: Activity["id"] } }
+  | { type: "delete-activity"; payload: { id: Activity["id"] } }
+  | { type: "restart-app" };
+
+export type ActvitySate = {
+  activities: Activity[];
+  activeId: Activity["id"];
+};
+
+const localStorageActivities = (): Activity[] => {
+  const activities = localStorage.getItem("activities");
+  return activities ? JSON.parse(activities) : [];
+};
+
+export const initialState: ActvitySate = {
+  activities: localStorageActivities(),
+  activeId: "",
+};
+
+export const activityReducer = (
+  state: ActvitySate = initialState,
+  action: ActivityActions
+) => {
+  if (action.type === "save-activity") {
+    let updatedActivities: Activity[] = [];
+
+    if (state.activeId) {
+      updatedActivities = state.activities.map((activity) =>
+        activity.id === state.activeId ? action.payload.newAcitvity : activity
+      );
+    } else {
+      updatedActivities = [...state.activities, action.payload.newAcitvity];
+    }
+
+    return {
+      ...state,
+      activities: updatedActivities,
+      activeId: "",
+    };
+  }
+
+  if (action.type === "set-activeId") {
+    return {
+      ...state,
+      activeId: action.payload.id,
+    };
+  }
+
+  if (action.type === "delete-activity") {
+    return {
+      ...state,
+      activities: state.activities.filter(
+        (activity) => activity.id !== action.payload.id
+      ),
+    };
+  }
+
+  if (action.type === "restart-app") {
+    return {
+      activities: [],
+      activeId: "",
+    };
+  }
+
+  return state;
+};
