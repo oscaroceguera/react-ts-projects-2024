9eb524608b295c8fd7480cde167cf955fa53728b
Oscar Oceguera bibriesca <oscaroceguera@gmail.com>
2024-08-12 23:10:39 -0700

    [UpTask] - FrontEnd: Create project

---
+++ uptask_frontend/src/api/ProjectAPI.tsx
@@ -0,0 +1,14 @@
+import { isAxiosError } from "axios";
+import { ProjectFormData } from "../types";
+import api from "@/lib/axios";
+
+export async function createProject(formData: ProjectFormData) {
+  try {
+    const { data } = await api.post("/projects", formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
