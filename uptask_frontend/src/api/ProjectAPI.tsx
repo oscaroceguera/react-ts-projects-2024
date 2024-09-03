b6c50d62c8015dd1acd1139482dbd7268e6c0a7f
Oscar Oceguera bibriesca <oscaroceguera@gmail.com>
2024-09-02 18:11:59 -0700

    UpTask: projects and edit projects

--- uptask_frontend/src/api/ProjectAPI.tsx
+++ uptask_frontend/src/api/ProjectAPI.tsx
@@ -1,10 +1,62 @@
 import { isAxiosError } from "axios";
-import { ProjectFormData } from "../types";
+import { Project, ProjectFormData, dashboardProjectSchema } from "../types";
 import api from "@/lib/axios";
 
 export async function createProject(formData: ProjectFormData) {
   try {
     const { data } = await api.post("/projects", formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getProjects() {
+  try {
+    const { data } = await api("/projects");
+    const response = dashboardProjectSchema.safeParse(data);
+    console.log("🚀 ~ getProjects ~ response:", response);
+    if (response.success) {
+      return response.data;
+    }
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getProjectById(id: Project["_id"]) {
+  try {
+    const { data } = await api(`/projects/${id}`);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+type ProjectApiTyoe = {
+  formData: ProjectFormData;
+  projectId: Project["_id"];
+};
+export async function updateProject({ formData, projectId }: ProjectApiTyoe) {
+  try {
+    const { data } = await api.put<string>(`/projects/${projectId}`, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function deleteProject(id: Project["_id"]) {
+  try {
+    const { data } = await api.delete<string>(`/projects/${id}`);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
